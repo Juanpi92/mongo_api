c9be24d80c28eaae26ac5f1c06fbedb8beaea767
Juanpi <juan.urgelles92@gmail.com>
2023-05-18 13:27:55 -0300

    creating Api process 2

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express  from "express";
+import dotenv from "dotenv";
 
 
+dotenv.config();
+const PORT = process.env.PORT||3000;
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 
 
 
 
+app.get("/",async (req, res) => {
+    res.status(200).send({message:"Api is going"})
+    });
 
 
 app.listen(PORT, () => {
