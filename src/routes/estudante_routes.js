5a8ba4361e6b8bd4311d210617a518a45987e7a6
Juanpi <juan.urgelles92@gmail.com>
2023-05-20 11:31:03 -0300

    creating all student option

--- src/routes/estudante_routes.js
+++ src/routes/estudante_routes.js
@@ -24,47 +24,44 @@ export const EstudanteRoutes = (app) => {
   });
 
   //Buscar
-  app.get("/person", async (req, res) => {
+  app.get("/estudante", async (req, res) => {
     try {
       let id = req.body.id;
-
-      //let person = await Person.find({ _id: id });
-      let person = await Person.findById(id);
-      if (!person) {
-        res.status(204).send({ message: "No encontramos o usuario" });
+      //let estudante = await Estudante.find({ _id: id });
+      let estudante = await Estudante.findById(id);
+      if (!estudante) {
+        res.status(204).send({ message: "No encontramos o Estudante" });
         return;
       }
-      res.status(200).send({ person });
+      res.status(200).send({ estudante });
     } catch (error) {
-      console.log(error);
       res.status(500).json({ error: error });
     }
   });
 
-  app.get("/Allperson", async (req, res) => {
+  app.get("/allEstudantes", async (req, res) => {
     try {
-      let people = await Person.find();
-      if (!people) {
-        res.status(204).send({ message: "No encontramos nehum usuario" });
+      let estudantes = await Estudante.find();
+      if (!estudantes) {
+        res.status(204).send({ message: "No encontramos nehum estudante" });
         return;
       }
-      res.status(200).send(people);
+      res.status(200).send(estudantes);
     } catch (error) {
-      console.log(error);
       res.status(500).json({ error: error });
     }
   });
 
   //Por posision
-  app.get("/person/:pos", async (req, res) => {
+  app.get("/estudante/:pos", async (req, res) => {
     try {
       const { pos } = req.params;
-      let person = await Person.findOne().skip(pos);
-      if (!person) {
-        res.status(204).send({ message: "No encontramos o usuario" });
+      let estudante = await Estudante.findOne().skip(pos);
+      if (!estudante) {
+        res.status(204).send({ message: "No encontramos o estudante" });
         return;
       }
-      res.status(200).send({ person });
+      res.status(200).send({ estudante });
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: error });
@@ -72,53 +69,55 @@ export const EstudanteRoutes = (app) => {
   });
 
   //Update
-  // Actualizar una Persona por su ID
-  app.put("/person/:id", async (req, res) => {
+  // Actualizar una estudante por su ID
+  app.put("/estudante/:id", async (req, res) => {
     try {
-      let person = await Person.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      });
-      if (!person) {
-        res.status(204).send({ message: "No encontramos o usuario" });
+      let estudante = await Estudante.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+        }
+      );
+      if (!estudante) {
+        res.status(204).send({ message: "No encontramos o estudante" });
         return;
       }
-      res.status(200).send({ message: "Usuario atualizado exitosamente" });
+      res.status(200).send({ message: "Estudante atualizado exitosamente" });
     } catch (error) {
-      console.error("Error al actualizar el ejemplo:", error);
-      res.status(500).json({ error: "Error al actualizar el ejemplo" });
+      res.status(500).json({ error: "Error al actualizar o estudante" });
     }
   });
 
   //Delete
-  app.delete("/person/:id", async (req, res) => {
+  app.delete("/estudante/:id", async (req, res) => {
     try {
-      let person = await Person.findByIdAndRemove(req.params.id);
-      //await Person.findByIdAndDelete(req.params.id);
-      if (!person) {
-        res.status(204).send({ message: "No encontramos o usuario" });
+      let estudante = await Estudante.findByIdAndRemove(req.params.id);
+      //await Estudante.findByIdAndDelete(req.params.id);
+      if (!estudante) {
+        res.status(204).send({ message: "No encontramos o estudante" });
         return;
       }
-      res.status(200).send({ message: "Usuario deletado exitosamente" });
+      res.status(200).send({ message: "Estudante deletado exitosamente" });
     } catch (error) {
-      console.error("Error al eliminar la persona:", error);
-      res.status(500).json({ error: "Error al eliminar la persona" });
+      res.status(500).json({ error: "Error ao eliminar o estudante" });
     }
   });
 
   //Patch
-  app.patch("/person/:id", async (req, res) => {
+  app.patch("/estudante/:id", async (req, res) => {
     try {
       const id = req.params.id;
       const updates = req.body;
       const options = { new: true }; // Devolver el documento actualizado
 
-      const person = await Person.findByIdAndUpdate(id, updates, options);
+      const estudante = await Estudante.findByIdAndUpdate(id, updates, options);
 
-      if (!person) {
-        res.status(204).send({ message: "No encontramos o usuario" });
+      if (!estudante) {
+        res.status(204).send({ message: "No encontramos o estudante" });
         return;
       }
-      res.status(200).send({ message: "Usuario deletado exitosamente" });
+      res.status(200).send({ message: "Estudante deletado exitosamente" });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: "Error en el servidor" });
